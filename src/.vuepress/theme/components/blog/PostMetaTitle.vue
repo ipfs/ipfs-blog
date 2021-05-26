9dcb9db5298aaef0e160c45a41c3d2b149888ed8
cwaring <chris@wwaves.co>
2021-05-26 23:52:10 +0100

    add PostMetaTitle

---
+++ src/.vuepress/theme/components/blog/PostMetaTitle.vue
@@ -0,0 +1,7 @@
+<template functional>
+  <span v-if="props.isExternal"
+    ><span class="mr-4">{{ props.title }}</span
+    ><OutboundLink class="-ml-4 pl-1" />
+  </span>
+  <span v-else>{{ props.title }}</span>
+</template>
