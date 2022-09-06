d333339e0bf29463fd820ee846dcad39ed565ad6
Daniel N <2color@users.noreply.github.com>
2022-09-06 19:24:43 +0200

    Use proxy

--- src/.vuepress/config/head.js
+++ src/.vuepress/config/head.js
@@ -30,9 +30,13 @@ module.exports = [
   ['meta', { name: 'msapplication-TileColor', content: '#156ff7' }],
   ['meta', { name: 'apple-mobile-web-app-title', content: 'IPFS Blog & News' }],
   ['meta', { name: 'application-name', content: 'IPFS Blog & News' }],
-  ['script', {
-    defer: true,
-    'data-domain': "blog.ipfs.tech", 
-    src: "https://plausible.io/js/plausible.js"
-  }]
+  [
+    'script',
+    {
+      defer: true,
+      'data-domain': 'blog.ipfs.tech',
+      'data-api': 'https://pl-proxy.daas.workers.dev/api/event',
+      src: 'https://pl-proxy.daas.workers.dev/js/script.js',
+    },
+  ],
 ].concat(favicons)
